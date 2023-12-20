d74c142d129e3df0923be76a24f811a625fc3077
abimanyu1510 <asalabimanyu15@gmail.com>
2023-12-20 20:24:26 +0530

    Task Completed

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 
-function Card(props) {
+function Card({cardData}) {
   return <>
      {
       <div className="col-lg-4">
         <div className="card mb-5 mb-lg-0">
           <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">{props.cardData.title}</h5>
-            <h6 className="card-price text-center">{props.cardData.price}<span className="period">/month</span></h6>
+            <h5 className="card-title text-muted text-uppercase text-center">{cardData.title}</h5>
+            <h6 className="card-price text-center">{cardData.price}<span className="period">/month</span></h6>
             <hr/>
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.cardData.features}</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.cardData.users}</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.cardData.projects}</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>{props.cardData. comunityAccess}</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{props.cardData.dataBack}</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{props.cardData.support}
-                </li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{props.cardData.subDomain}
-              </li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{props.cardData.status}</li>
+              <li className={cardData.isFeatures?"":"text-muted"}><span className="fa-li"><i className={cardData.isFeatures?"fas fa-check":"fas fa-times"}></i></span>{cardData.title!=="FREE"?<b>{cardData.features}</b>:<>{cardData.features}</>}</li>
+              <li className={cardData.isUsers?"":"text-muted"}><span className="fa-li"><i className={cardData.isUsers?"fas fa-check":"fas fa-times"}></i></span>{cardData.users}</li>
+              <li className={cardData.isDataBack?"":"text-muted"}><span className="fa-li"><i className={cardData.isDataBack?"fas fa-check":"fas fa-times"}></i></span>{cardData.dataBack}</li>
+              <li className={cardData.isCommunityAccess?"":"text-muted"}><span className="fa-li"><i className={cardData.isCommunityAccess?"fas fa-check":"fas fa-times"}></i></span>{cardData. communityAccess}</li>
+              <li className={cardData.isProjects?"":"text-muted"}><span className="fa-li"><i className={cardData.isProjects?"fas fa-check":"fas fa-times"}></i></span>{cardData.projects}</li>
+              <li className={cardData.isSupport?"":"text-muted"}><span className="fa-li"><i className={cardData.isSupport?"fas fa-check":"fas fa-times"}></i></span>{cardData.support}</li>
+              <li className={cardData.isSubDomain?"":"text-muted"}><span className="fa-li"><i className={cardData.isSubDomain?"fas fa-check":"fas fa-times"}></i></span>{cardData.title==="PRO"?<><b>Unlimitted </b> {cardData.subDomain}</>:cardData.subDomain}</li>
+              <li className={cardData.isStatus?"":"text-muted"}><span className="fa-li"><i className={cardData.isStatus?"fas fa-check":"fas fa-times"}></i></span>{cardData.status}</li>
             </ul>
+
             <div className="d-grid">
               <a href="#" className="btn btn-primary text-uppercase">Button</a>
             </div>
